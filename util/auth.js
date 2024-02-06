117b7f5d8c3a924f1ddac7e7a1921e1778306a74
Rick <ricardo.henriques@accedo.tv>
2024-02-06 11:04:11 +0000

    auth util

---
+++ util/auth.js
@@ -0,0 +1,24 @@
+import axios from axios;
+
+const API_KEY = "AIzaSyCTmwHMrC8F3Kaj3ZL9y77CU6D63fC51lU";
+
+export async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=` + API_KEY
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true
+  });
+
+  const token = response.data.idToken;
+  return token
+}
+
+export function createUser(email, password) {
+  return authenticate("signup", email, password)
+}
+
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password)
+}
